92bbc163d251d85b9c959dbbf7af59133aab2e74
n0rrman <email@henriknorrman.com>
2024-09-03 21:02:18 +0200

    day 23 loading data succesfully

--- 23/main.go
+++ 23/main.go
@@ -7,19 +7,31 @@ import (
 	"strings"
 )
 
-func readData(file string) []string {
+func readData(file string) [][]string {
 	body, err := os.ReadFile(file)
 	if err != nil {
 		log.Fatalf("unable to read file: %v", err)
 	}
 	sData := strings.Split(string(body[:]), "\n")
 
-	return sData
+	topData := strings.Split(sData[2], "#")
+	bottomData := strings.Split(sData[3], "#")
+
+	fmt.Println(topData[3], topData[4], topData[5], topData[6])
+	fmt.Println(bottomData[1], bottomData[2], bottomData[3], bottomData[4])
+
+	rooms := [][]string{
+		{topData[3], bottomData[1]},
+		{topData[4], bottomData[2]},
+		{topData[5], bottomData[3]},
+		{topData[6], bottomData[4]},
+	}
+	return rooms
 }
 
 func main() {
-	data := readData("data")
-	_ = data
+	rooms := readData("test_data")
+	fmt.Println(rooms)
 
 	// Part One
 	results := "part one"
