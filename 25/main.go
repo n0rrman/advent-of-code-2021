e09a0cb8093936d7bf9210a5d1a9317cdce3d70f
n0rrman <email@henriknorrman.com>
2024-09-05 23:49:16 +0200

    day 25 loading data succesfully

--- 25/main.go
+++ 25/main.go
@@ -7,19 +7,36 @@ import (
 	"strings"
 )
 
-func readData(file string) []string {
+func readData(file string) [][]int {
 	body, err := os.ReadFile(file)
 	if err != nil {
 		log.Fatalf("unable to read file: %v", err)
 	}
 	sData := strings.Split(string(body[:]), "\n")
 
-	return sData
+	data := make([][]int, len(sData))
+
+	for y := range data {
+		data[y] = make([]int, len(sData[y]))
+		for x, val := range strings.Split(sData[y], "") {
+			var code int
+			switch val {
+			case ".":
+				code = 0
+			case ">":
+				code = 1
+			case "v":
+				code = 2
+			}
+			data[y][x] = code
+		}
+	}
+	return data
 }
 
 func main() {
-	data := readData("data")
-	_ = data
+	data := readData("test_data")
+	fmt.Println(data)
 
 	// Part One
 	results := "part one"
