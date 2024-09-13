13612d7063e32e5ae603a97647312c6245a4dec4
n0rrman <email@henriknorrman.com>
2024-09-13 23:56:19 +0200

    day 19 data loading added

--- 19/main.go
+++ 19/main.go
@@ -4,22 +4,41 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
-func readData(file string) []string {
+func readData(file string) [][][]int {
 	body, err := os.ReadFile(file)
 	if err != nil {
 		log.Fatalf("unable to read file: %v", err)
 	}
-	sData := strings.Split(string(body[:]), "\n")
+	sData := strings.Split(string(body[:]), "\n\n")
+	scanners := make([][][]int, len(sData))
 
-	return sData
+	for k, scannerData := range sData {
+		scanners[k] = make([][]int, len(sData[0]))
+		for i, row := range strings.Split(scannerData, "\n") {
+			if i == 0 {
+				continue
+			}
+			intRow := make([]int, 3)
+			for j, val := range strings.Split(row, ",") {
+				intRow[j], _ = strconv.Atoi(val)
+			}
+			scanners[k][i-1] = intRow
+		}
+	}
+	return scanners
 }
 
 func main() {
-	data := readData("data")
-	_ = data
+	data := readData("test_data")
+
+	for _, scanner := range data {
+		fmt.Println(scanner)
+
+	}
 
 	// Part One
 	results := "part one"
