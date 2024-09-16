66b718455ae067bd5d4cbda297f0592595d51065
n0rrman <email@henriknorrman.com>
2024-09-16 21:18:52 +0200

    day 19 part 1: beacons and 3d distances calculated per scanner

--- 19/a.go
+++ 19/a.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 type scanner struct {
 	beacons   [][]int
@@ -8,24 +11,38 @@ type scanner struct {
 }
 
 func (s *scanner) init(beacons [][]int) {
-	copy(s.beacons, beacons)
+	(*s).beacons = make([][]int, len(beacons))
+	for i, b := range beacons {
+		(*s).beacons[i] = make([]int, 3)
+		copy((*s).beacons[i], b)
+	}
+
+	(*s).calcDistances()
 }
 
 func (s *scanner) calcDistances() {
-	// Calculate 3d distances
+	(*s).distances = make(map[[2]int]float64)
+	for i := 0; i < len(s.beacons); i++ {
+		for j := i + 1; j < len(s.beacons); j++ {
+			(*s).distances[[2]int{i, j}] = calc3dDistance(s.beacons[i], s.beacons[j])
+		}
+	}
+}
+
+func calc3dDistance(a []int, b []int) float64 {
+	tmp := math.Pow(float64(a[0])-float64(b[0]), 2) + math.Pow(float64(a[1])-float64(b[1]), 2) + math.Pow(float64(a[2])-float64(b[2]), 2)
+	return math.Sqrt(tmp)
 }
 
 func syncAndCountScanners(scanners [][][]int) int {
 	allScanners := make([]scanner, len(scanners))
 
 	for i, s := range scanners {
-		fmt.Println(i)
 		newScanner := scanner{}
 		newScanner.init(s)
-		newScanner.calcDistances()
 		allScanners[i] = newScanner
 	}
-	fmt.Println(allScanners)
-	//fmt.Println(beacons[1], scanners[3][1])
+	fmt.Println(allScanners[0].beacons)
+	fmt.Println(allScanners[0].distances)
 	return 0
 }
