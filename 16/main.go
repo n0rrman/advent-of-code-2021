ea38b8bb4ca201f2bc0c04abd9a91eadfde788e6
n0rrman <email@henriknorrman.com>
2024-09-21 20:32:56 +0200

    day 16 hex to bit implemented

--- 16/main.go
+++ 16/main.go
@@ -17,9 +17,36 @@ func readData(file string) []string {
 	return sData
 }
 
+func hexToBin(data []string) []uint8 {
+	var bitArray []uint8
+	fourBitConv := make(map[string][]uint8)
+	fourBitConv["0"] = []uint8{0, 0, 0, 0}
+	fourBitConv["1"] = []uint8{0, 0, 0, 1}
+	fourBitConv["2"] = []uint8{0, 0, 1, 0}
+	fourBitConv["3"] = []uint8{0, 0, 1, 1}
+	fourBitConv["4"] = []uint8{0, 1, 0, 0}
+	fourBitConv["5"] = []uint8{0, 1, 0, 1}
+	fourBitConv["6"] = []uint8{0, 1, 1, 0}
+	fourBitConv["7"] = []uint8{0, 1, 1, 1}
+	fourBitConv["8"] = []uint8{1, 0, 0, 0}
+	fourBitConv["9"] = []uint8{1, 0, 0, 1}
+	fourBitConv["A"] = []uint8{1, 0, 1, 0}
+	fourBitConv["B"] = []uint8{1, 0, 1, 1}
+	fourBitConv["C"] = []uint8{1, 1, 0, 0}
+	fourBitConv["D"] = []uint8{1, 1, 0, 1}
+	fourBitConv["E"] = []uint8{1, 1, 1, 0}
+	fourBitConv["F"] = []uint8{1, 1, 1, 1}
+
+	for _, hex := range data {
+		bitArray = append(bitArray, fourBitConv[hex]...)
+	}
+	return bitArray
+}
+
 func main() {
 	data := readData("data")
-	fmt.Println(data)
+	bits := hexToBin(data)
+	fmt.Println(bits)
 
 	// Part One
 	results := "part one"
--- 16/main_test.go
+++ 16/main_test.go
@@ -1,10 +1,34 @@
 package main
 
 import (
+	"strconv"
+	"strings"
 	"testing"
 )
 
+func TestHexToBin(t *testing.T) {
+	hex := strings.Split("38006F45291200", "")
+	converted := hexToBin(hex)
+
+	bin := strings.Split("00111000000000000110111101000101001010010001001000000000", "")
+
+	fullMatch := true
+	for i := range bin {
+		bit, _ := strconv.Atoi(bin[i])
+		if converted[i] != uint8(bit) {
+			fullMatch = false
+			break
+		}
+	}
+
+	if !fullMatch {
+		t.Error("Elements does not match!")
+	}
+}
+
 func TestA(t *testing.T) {
+	data := readData("test_data")
+	_ = data
 	results := 0
 
 	const e = 1
