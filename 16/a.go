814c49a4ca4f7eeadc201955a1d50a61a95a89e3
n0rrman <email@henriknorrman.com>
2024-09-27 23:45:20 +0200

    day 16 part a: initial packet rework

--- 16/a.go
+++ 16/a.go
@@ -6,9 +6,10 @@ import (
 )
 
 type packet struct {
-	version int
-	typeId  int
-	length  int
+	version  int
+	typeId   int
+	lengthId int
+	literal  int
 }
 
 func bitsToNum(bits []uint8) int {
@@ -19,8 +20,16 @@ func bitsToNum(bits []uint8) int {
 	return val
 }
 
-func evalLiteral(bits []uint8) (int, int, bool) {
-	return 1, 3, false
+func evalLiteral(bits []uint8) int {
+	i := 0
+	moreBits := true
+	var bitVal []uint8
+	for moreBits {
+		bitVal = append(bitVal, bits[i+1:i+5]...)
+		moreBits = bits[i] == 1
+		i += 5
+	}
+	return bitsToNum(bitVal)
 }
 
 func bitsToPackets(bits []uint8) packet {
@@ -32,22 +41,18 @@ func bitsToPackets(bits []uint8) packet {
 		newPacket.version = bitsToNum(bits[index : index+3])
 		newPacket.typeId = bitsToNum(bits[index+3 : index+6])
 
-		fmt.Println()
-		//fmt.Println(float64())
-
 		if newPacket.typeId == 4 {
-			var numGroups int
-			_, numGroups, moreBits = evalLiteral(bits[index+6:])
-			index = int(float64(index+(numGroups*5)+6) / float64(5))
-
-			fmt.Println(index)
+			//literal, evalIndex := evalLiteral(bits[index+6:])
+			//index = index + evalIndex
+			newPacket.lengthId = 0
+			//newPacket.literal = literal
 		}
-		//packetIndex++
 		moreBits = false
 	}
 
 	fmt.Println(newPacket.version)
 	fmt.Println(newPacket.typeId)
+	fmt.Println(newPacket.literal)
 
 	return newPacket
 }
@@ -55,10 +60,32 @@ func bitsToPackets(bits []uint8) packet {
 // type 4: literal value => list of packs, 1 -> keep reading, 0 -> last pack
 // type !4: operator => length bit = 0 => 15, 1 => next 11 decide number of bits
 
-func calcVersionSum(bits []uint8) int {
+func recCalcVersionSum(bits []uint8) int {
+	version := bitsToNum(bits[0:3])
+	typeId := bitsToNum(bits[3:6])
 
-	packets := bitsToPackets(bits)
-	_ = packets
-
+	// type 4 -> Literal
+	var literal int
+	if typeId == 4 {
+		literal = evalLiteral(bits[6:])
+		fmt.Println("literal: ", literal)
+	} else {
+		lengthId := bits[6]
+		var length int
+		// Number of bits
+		if lengthId == 0 {
+			length = bitsToNum(bits[7 : 7+15])
+			fmt.Println("num bits: ", length)
+		}
+		// Number of subpackets
+		if lengthId == 1 {
+			length = bitsToNum(bits[7 : 7+11])
+			fmt.Println("num subpacks:", length)
+		}
+	}
 	return 0
+}
+
+func calcVersionSum(bits []uint8) int {
+	return recCalcVersionSum(bits)
 }
