a0ab81c9a99fa018e2e34132b62010e3a64aa499
n0rrman <mail@henriknorrman.com>
2024-10-06 22:31:27 +0200

    day 22 data loading succesfully

--- 22/main.go
+++ 22/main.go
@@ -5,20 +5,46 @@ import (
 	"log"
 	"os"
 	"strings"
+	"strconv"
 )
 
-func readData(file string) []string {
+type Range [2]int 
+type instruction struct {
+	Mode bool
+	Coords [3]Range
+}
+
+
+func readData(file string) []instruction {
 	body, err := os.ReadFile(file)
 	if err != nil {
 		log.Fatalf("unable to read file: %v", err)
 	}
 	sData := strings.Split(string(body[:]), "\n")
 
-	return sData
+	instructions := make([]instruction, len(sData))
+	for i, row := range sData {
+		modeSplit := strings.Split(row, " ")
+		
+		instructions[i] = instruction{}	
+		instructions[i].Mode = modeSplit[0] == "on"
+
+		for c, coord := range strings.Split(modeSplit[1], ",") {
+			cleanUp := strings.Split(coord, "=")
+			coords := strings.Split(cleanUp[1], "..")
+			c1, _ := strconv.Atoi(coords[0])
+			c2, _ := strconv.Atoi(coords[1])
+			instructions[i].Coords[c] = Range{c1, c2}
+		
+		}
+	}
+
+	return instructions
 }
 
 func main() {
-	data := readData("data")
+	data := readData("test_data2")
+	fmt.Println(data)
 	_ = data
 
 	// Part One
