83a07f90946c98654884ea0af912649855e1154b
n0rrman <mail@henriknorrman.com>
2024-10-08 23:53:04 +0200

    day 22 part 1: x1>x2 reordering

--- 22/a.go
+++ 22/a.go
@@ -18,8 +18,20 @@ func countCubes(reactor [][][]bool) int {
 	return count
 }
 
+func normalise(i []instruction) []instruction {
+	for index, instr := range i {
+		for c, coord := range instr.Coords {
+			if coord[0] > coord[1] {
+				i[index].Coords[c] = [2]int{coord[1], coord[0]}
+			}
+		}
+	}
+	return i
+}
+
 func rebootAndCount(i []instruction) int {
-	reactor := bootUp(i)
+	instructions := normalise(i)
+	reactor := bootUp(instructions)
 	numOfCubes := countCubes(reactor)
 	return numOfCubes
 }
